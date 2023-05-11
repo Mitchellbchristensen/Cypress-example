4fb7ad465ad6193e0df26b0f4e34881f825440c5
Mitchellbchristensen <52010189+Mitchellbchristensen@users.noreply.github.com>
2023-05-11 13:51:15 -0600

    Delete Mitchell-cypress-project - directory

--- Mitchell-cypress-project -/cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'https://commitquality.com/practice-contact-form',
-    setupNodeEvents(on, config) {
-    },
-  },
-});
